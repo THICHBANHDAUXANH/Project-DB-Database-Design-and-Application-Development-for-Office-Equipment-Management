b83c06e11b88e67dd60045628d05ed6522926fc0
buiduyanh@@@ <110718372+THICHBANHDAUXANH@users.noreply.github.com>
2026-04-26 10:38:49 -0700

    Update config.py

--- python/config.py
+++ python/config.py
@@ -1,17 +1,7 @@
-import os
-
-
 DB_CONFIG = {
-    "host": os.getenv("OEM_DB_HOST", "localhost"),
-    "port": int(os.getenv("OEM_DB_PORT", "3306")),
-    "user": os.getenv("OEM_DB_USER", "root"),
-    "password": os.getenv("OEM_DB_PASSWORD", ""),
-    "database": os.getenv("OEM_DB_NAME", "office_equipment_management"),
+    "host": "localhost",
+    "port": "yourport,
+    "user": "root",
+    "password": "your_mysql_password",
+    "database": "office_equipment_management",
 }
-
-try:
-    from config_local import DB_CONFIG as LOCAL_DB_CONFIG
-
-    DB_CONFIG.update(LOCAL_DB_CONFIG)
-except ImportError:
-    pass
